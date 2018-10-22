d6d57f3393d5cc7578364421fc0160df5b2bdd25
akashks1998 <akashks@iitk.ac.in>
2018-10-23 05:21:28 +0530

    Basic sceduler implemented

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fixed');
+  let startt=new Date();
+  startt=startt.toISOString();
+  startt=startt.slice(0, -8);
+  console.log(new Date());
+  
+  res.render('fixed',{start:startt});
 });
 router.get('/fixed', function(req, res, next) {
-  res.render('fixed');
+  let startt=new Date();
+  startt=startt.toISOString();
+  startt=startt.slice(0, -8);
+  res.render('fixed',{start:startt.toISOString()});
 });
 router.get('/deadline', function(req, res, next) {
   res.render('deadline');
