460b83b1dc318d2d92149d04c3031c7842783430
akashks1998 <akashks@iitk.ac.in>
2018-10-27 12:28:12 +0530

    Some changes in scheduler

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
+var express = require("express");
+var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-var crypto = require('crypto');
+MongoClient.connect(
+  url,
+  function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  }
+);
+var crypto = require("crypto");
 var router = express.Router();
 class user {
   constructor(nam, pas) {
     this.nam = nam;
-    this.pas = crypto.createHash('md5').update(pas).digest('hex');
+    this.pas = crypto
+      .createHash("md5")
+      .update(pas)
+      .digest("hex");
     this.fixed = [];
     this.deadline = [];
     this.scedule = [];
     this.err = 0;
+    this.hour = 0;
   }
 
   addfixed(start, end, name) {
@@ -31,23 +38,21 @@ class user {
       }
     });
     this.fixed.push([start, end, name]);
-    this.fixed.sort(
-      function (a, b) {
-        return a[0] > b[0] ? 1 : -1;
-      });
+    this.fixed.sort(function(a, b) {
+      return a[0] > b[0] ? 1 : -1;
+    });
     this.sceduler();
   }
 
   adddead(deadline, hour, name) {
-    if(hour<0){
+    if (hour < 0) {
       this.err++;
       return;
     }
     this.deadline.push([deadline, hour, name]);
-    this.deadline.sort(
-      function (a, b) {
-        return a[0] > b[0] ? 1 : -1;
-      });
+    this.deadline.sort(function(a, b) {
+      return a[0] > b[0] ? 1 : -1;
+    });
     this.sceduler();
   }
   sceduler() {
@@ -58,45 +63,71 @@ class user {
       this.scedule = [0, 0, "Sleep"];
       return;
     }
-    if (this.deadline.length!=0&&(this.fixed.length==0||this.x < this.fixed[0][0])) {
-      if (this.deadline[0][1] <= 0) {
-        this.deadline.shift();
-        this.sceduler();
+    if ( this.deadline.length != 0 && (this.fixed.length == 0 || this.x < this.fixed[0][0])    ) {
+      if (this.hour < 3 || this.deadline.length < 2) {
+        if (this.deadline[0][1] <= 0) {
+          this.deadline.shift();
+          this.sceduler();
+          return;
+        }
+        if (this.deadline[0][0] < this.x) {
+          this.errr = 1 + this.err;
+          this.deadline.shift();
+          this.sceduler();
+          return;
+        }
+        this.deadline[0][1] = this.deadline[0][1] - 1;
+        this.scedule = this.deadline[0];
+        this.hour=(this.hour+1)%3;
+        return;
+      }else{
+        if (this.deadline[1][1] <= 0) {
+          this.deadline.splice(1,1);
+          this.sceduler();
+          return;
+        }
+        if (this.deadline[1][0] < this.x) {
+          this.errr = 1 + this.err;
+          this.deadline.splice(1,1);
+          this.sceduler();
+          return;
+        }
+        this.deadline[1][1] = this.deadline[0][1] - 1;
+        this.scedule = this.deadline[0];
+        this.hour=(this.hour+1)%3;
         return;
       }
-      if (this.deadline[0][0] < this.x) {
-        this.errr = 1 + this.err;
-        this.deadline.shift();
-        this.sceduler();
-        return;
-      }
-      this.deadline[0][1] = this.deadline[0][1] - 1;
-      this.scedule = this.deadline[0];
-      return;
-
-    } else if (this.fixed.length!=0&&this.x >= this.fixed[0][0] && this.x <= this.fixed[0][1]) {
+    } else if (
+      this.fixed.length != 0 &&
+      this.x >= this.fixed[0][0] &&
+      this.x <= this.fixed[0][1]
+    ) {
       this.scedule = this.fixed[0];
-    } else if (this.fixed.length!=0&&this.x > this.fixed[0][1]) {
+    } else if (this.fixed.length != 0 && this.x > this.fixed[0][1]) {
       this.fixed.shift();
       this.sceduler();
       return;
     }
   }
-};
+}
 let u1 = new user("akash", "password");
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function(req, res, next) {
   res.send(JSON.stringify(u1));
 });
 
-router.post('/', function (req, res, next) {
+router.post("/", function(req, res, next) {
   if (req.body.start && req.body.end) {
-    u1.addfixed(new Date(req.body.start), new Date(req.body.end), req.body.name);
+    u1.addfixed(
+      new Date(req.body.start),
+      new Date(req.body.end),
+      req.body.name
+    );
   }
-  if (req.body.deadline != '' && req.body.hours) {
+  if (req.body.deadline != "" && req.body.hours) {
     u1.adddead(new Date(req.body.deadline), req.body.hours, req.body.name);
   }
   res.send(JSON.stringify(u1));
 });
-setInterval(u1.sceduler,3600000);
+setInterval(u1.sceduler, 3600000);
 module.exports = router;
